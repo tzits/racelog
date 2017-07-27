90fdda3dc6439493ff5125650043aa522ccab00f
tzits <tzitsman@gmail.com>
2017-07-27 15:47:46 -0400

    completes basic user controller skeleton

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,34 @@ class UsersController < ApplicationController
     else
       explode
     end
-    
+
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      redirect_to :back
+      # flash[:error] = @user.errors.full_messages.join(". ")
+    end
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to root_path
+    else
+      explode
+    end
   end
 
   private
