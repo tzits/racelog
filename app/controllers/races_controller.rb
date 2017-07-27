210e3013872af1e3290564b94f3c78b37ab3950a
tzits <tzitsman@gmail.com>
2017-07-27 15:50:37 -0400

    creates races contollers skeleton

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,44 @@
 class RacesController < ApplicationController
+  def create
+    @race = Race.new(race_params)
+    if @race.valid?
+      @race.save
+    else
+      explode
+    end
+
+  end
+
+  def show
+    @race = Race.find(params[:id])
+  end
+
+  def edit
+    @race = Race.find(params[:id])
+  end
+
+  def update
+    @race = Race.find(params[:id])
+    if @race.update(race_params)
+      redirect_to :back
+    else
+      redirect_to :back
+      # flash[:error] = @user.errors.full_messages.join(". ")
+    end
+  end
+
+  def destroy
+    @race = Race.find(params[:id])
+    if @race.destroy
+      redirect_to root_path
+    else
+      explode
+    end
+  end
+
+  private
+
+  def race_params
+    params.require(:race).permit(:name, :distance, :time)
+  end
 end
