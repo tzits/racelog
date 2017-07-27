b3530ce9c42022de8cdae91d2cb3e44a0191e5e2
tzits <tzitsman@gmail.com>
2017-07-27 15:45:19 -0400

    creates create user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+    else
+      explode
+    end
+    
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :age, :email, :password)
+  end
 end
