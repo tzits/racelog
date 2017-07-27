45540de11f119099c3e0a1ca05d14b7c469af1ed
tzits <tzitsman@gmail.com>
2017-07-27 16:18:37 -0400

    remove unnecessary views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.confirm(params[:email], params[:password])
+
+    if @user
+      login(@user)
+      flash[:success] = "Login Successful"
+      redirect_to @user
+    else
+      flash[:error] = "Incorrect login or password"
+      redirect_to root_path
+    end
+
   end
 
   def destroy
+    logout
+    flash[:success] = "Bye for Now!"
+    redirect_to root_path
   end
 end
